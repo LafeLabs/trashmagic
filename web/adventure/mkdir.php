dca145b84943462b35e4ab3efa0e0a7201352b2a
Trash Robot <lafelabs@gmail.com>
2023-01-20 17:21:44 -0700

    forking function in web/adventure

--- web/adventure/mkdir.php
+++ web/adventure/mkdir.php
@@ -20,7 +20,24 @@ echo "<a href = \"".$dirname."/replicator.php\">".$dirname."/replicator.php</a>"
 
 ?>
 <style>
-    a{
-        font-size:3em;
-    }
+body{
+    background-color:black;
+}
+a{
+    text-align:center;
+    font-family:helvetica;
+    font-size:3em;
+    width:80%;
+    margin:auto;
+    display:block;
+    border:solid;
+    border-radius:0.5em;
+    border-width:0.5em;
+    color:black;
+    margin-top:1em;
+    margin-bottom:0.5em;
+    overflow-x:scroll;
+    background-color:red;
+    border-color:red;
+}
 </style>
